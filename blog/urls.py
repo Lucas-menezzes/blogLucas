f90fc62e615bf2fea05d561f0c1fd6450f3d8228
Lucas Menezes <lu289997@gmail.com>
2024-01-04 19:55:56 -0300

    criando templates documents e contacts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, project, document, contact
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='home'),
+    path('project/', project, name='project'),
+    path('document/', document, name='document'),
+    path('contact/', contact, name='contact')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'blog/index.html')
+
+def project(request):
+    return render(request, 'blog/project.html')
+
+def document(request):
+    return render(request, 'blog/documents.html')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
